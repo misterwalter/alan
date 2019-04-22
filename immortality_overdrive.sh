f1a3efee6028ae73540a642dc2f309016ac67ff2
Walter <verburg.walt@alumni.uwlax.edu>
2019-04-22 00:15:25 +0000

    Fix immortality_overdrive with respect to latest refactors

--- immortality_overdrive.sh
+++ immortality_overdrive.sh
@@ -1,1 +1,1 @@
-while :; do python3 al.py; echo "Alan has fallen!"; date; sleep 1; echo "Engage Immortality Overdrive!"; done
+while :; do python3 Alan.py; echo "Alan has fallen!"; date; sleep 1; echo "Engage Immortality Overdrive!"; done
