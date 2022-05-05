a754c1d27c79a713c00e2ee93353c7a98fe54dbf
cbrochtrup <cbrochtrup@greenkeytech.com>
2022-05-05 15:21:55 -0500

    Move git pull for immortal overlord.

--- immortality_overdrive.sh
+++ immortality_overdrive.sh
@@ -1,8 +1,8 @@
-while :;
-    do python3 Alan.py;
+while :; do
+    git pull
+    python3 Alan.py;
     echo "Alan has fallen!";
     date;
     sleep 1;
-    git pull
     echo "Engage Immortality Overdrive!";
 done
