7333c2983d215485ad4cdc84a28635c31b563577
cbrochtrup <cbrochtrup@greenkeytech.com>
2022-05-05 15:24:19 -0500

    Fix things again.

--- immortality_overdrive.sh
+++ immortality_overdrive.sh
@@ -1,8 +1,8 @@
 while :; do
     git pull
-    pipenv run Alan.py;
-    echo "Alan has fallen!";
-    date;
-    sleep 1;
-    echo "Engage Immortality Overdrive!";
+    pipenv run python3 Alan.py
+    echo "Alan has fallen!"
+    date
+    sleep 1
+    echo "Engage Immortality Overdrive!"
 done
